e15a0ca06fb341f048bb05d11b9fa58a35fce458
Ondrej Lukasek <ondrej@lukasek.cz>
2023-07-02 12:36:01 +0200

    submitForm update

--- src/components/SubmitForm.jsx
+++ src/components/SubmitForm.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 export default function SubmitForm() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const submittedText = e.target.inputField.value;
+        onFormSubmit(submittedText);
+        e.target.reset();
+    };
+
     return (
-        <>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="inputField">TYPE SMTH</label>
-            <input id="inputField" name="inputField" type="text"></input>
+            <input
+                id="inputField"
+                name="inputField"
+                placeholder="type recipe name"
+                type="text"
+            />
             <button type="submit">SUBMIT PLS</button>
-        </>
+        </form>
     );
 }
