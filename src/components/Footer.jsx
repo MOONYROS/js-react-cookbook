700649afd9e23f1acd482b2de0e169dd6fe9461f
Ondrej Lukasek <ondrej@lukasek.cz>
2023-07-12 18:08:22 +0200

    file for importing links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import * as links from "../webLinks";
 
 export default function Footer() {
     return (
         <>
             <h2>Footer</h2>
             <div>
-                Here might be some ending info like authors, contacts, links
+                <label htmlFor="">
+                    Made by <a href={links.profileLink}>MOONYROS</a>, 2023
+                </label>
             </div>
         </>
     );
