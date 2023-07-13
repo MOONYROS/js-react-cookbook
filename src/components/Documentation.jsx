7dd95e3b095c009611210dacfb84faeb35a5a8ec
Ondrej Lukasek <ondrej@lukasek.cz>
2023-07-13 20:55:21 +0200

    Documenatation heading

--- src/components/Documentation.jsx
+++ src/components/Documentation.jsx
@@ -4,7 +4,15 @@ export default function Documentation() {
     return (
         <>
             <h1>Documentation</h1>
-            <div>Documentation for the application.</div>
+            <div>
+                <h2>About this app</h2>
+                <p>
+                    This application works very basically using
+                    <label>arrays</label>, meaning that if you close the app and
+                    start it again, you will lose all you progress and
+                    everything you've done.
+                </p>
+            </div>
         </>
     );
 }
