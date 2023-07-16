c924f74af087e4b4e2f8834a748506fd5a843136
Ondrej Lukasek <ondrej@lukasek.cz>
2023-07-16 19:44:10 +0200

    very basic header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { containerStyle } from "../webLinks";
+
+const topHeaderStyle = { marginTop: "0", paddingTop: "20px" };
+const background = { backgroundColor: "#FADE7A" };
+const listElement = {
+    display: "inline-block",
+    padding: "20px",
+    margin: "10px",
+    borderRadius: "10px",
+    border: "1px solid red",
+};
 
 export default function Header() {
     return (
-        <div style={{ backgroundColor: "#FADE7A" }}>
-            <h1
-                style={{
-                    marginTop: "0",
-                    paddingTop: "20px",
-                }}
-            >
-                React cookbook 🍝 in Javascript 🟨
-            </h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/documentation">Documentation</Link>
-                    </li>
-                </ul>
-            </nav>
+        <div style={background}>
+            <div style={containerStyle}>
+                <h1 style={topHeaderStyle}>
+                    React cookbook 🍝 in Javascript 🟨
+                </h1>
+                <nav>
+                    <ul>
+                        <li style={listElement}>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li style={listElement}>
+                            <Link to="/documentation">Documentation</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 }
--- src/webLinks.js
+++ src/webLinks.js
@@ -1,1 +1,6 @@
 export const profileLink = "https://github.com/MOONYROS";
+
+export const containerStyle = {
+    width: "80%",
+    margin: "auto",
+};
