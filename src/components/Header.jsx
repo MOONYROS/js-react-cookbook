d9b8ae72e4ab636cd3bd183ffeb73ac3b3ce979c
Ondrej Lukasek <ondrej@lukasek.cz>
2023-07-17 22:57:51 +0200

    transfee to external stylesheet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { containerStyle } from "../webLinks";
-
-const topHeaderStyle = { marginTop: "0", paddingTop: "20px" };
-const background = { backgroundColor: "#FADE7A" };
-const listElement = {
-    display: "inline-block",
-    padding: "20px",
-    margin: "10px",
-    borderRadius: "10px",
-    border: "1px solid red",
-};
 
 export default function Header() {
     return (
-        <div style={background}>
-            <div style={containerStyle}>
-                <h1 style={topHeaderStyle}>
-                    React cookbook 🍝 in Javascript 🟨
-                </h1>
+        <div className="coloredBackground">
+            <div className="container">
+                <h1 id="headerHeading">React cookbook 🍝 in Javascript 🟨</h1>
                 <nav>
                     <ul className="something">
-                        <li style={listElement}>
+                        <li className="listElement">
                             <Link to="/">Home</Link>
                         </li>
-                        <li style={listElement}>
+                        <li className="listElement">
                             <Link to="/documentation">Documentation</Link>
                         </li>
                     </ul>
