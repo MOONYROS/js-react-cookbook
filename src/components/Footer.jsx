250325c675a26fcef6e7453e3e4fa58b0f05614d
Ondrej Lukasek <ondrej@lukasek.cz>
2023-07-18 23:12:04 +0200

    targetting change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,17 @@ import * as links from "../webLinks";
 
 export default function Footer() {
     return (
-        <div>
-            <h2>Footer</h2>
-            <p>
-                Made by <a href={links.profileLink}>MOONYROS</a>, 2023
-            </p>
+        <div className="coloredBackground" id="footer">
+            <div className="container">
+                <h2>Footer</h2>
+                <p>
+                    Made by{" "}
+                    <a href={links.profileLink} target="blank">
+                        MOONYROS
+                    </a>
+                    , 2023
+                </p>
+            </div>
         </div>
     );
 }
